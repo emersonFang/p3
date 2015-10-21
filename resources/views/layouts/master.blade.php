3c760b02bce1f1020dd82f1ddc3b05d76dad0a0a
Emerson Fang <efang@seas.harvard.edu>
2015-10-21 03:09:26 -0400

    separated styles from views into css stylesheet in public/assets/css directory, updated logo to something less ugly, updated homepage css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,179 +10,12 @@
 
     <meta charset='utf-8'>
 
-
     <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/readable/bootstrap.min.css" type="text/css">
-    <style>
+    <link rel="stylesheet" type="text/css" href="/assets/css/generatorpages.css">
 
-        html {
-            box-sizing: border-box;
-        }
-
-        *, *:before, *:after {
-            box-sizing: inherit;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        h2, h3 {
-            text-align: center;
-            padding:0;
-            border:0;
-        }
-
-        img.displayed {
-            display: block;
-            margin-left: auto;
-            margin-right: auto
-        }
-        .container {
-            width:80%;
-            padding: 0;
-            min-width:800px;
-            margin:auto;
-            text-align:center;
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        .results_container {
-            width:70%;
-            height: 200px;
-            border:1px solid black;
-            padding: 0%;
-            min-width:800px;
-            margin:auto;
-            text-align:left;
-            font-size: 12px;
-            font-weight: bold;
-            overflow:auto;
-            border-radius: 15px;
-
-        }
-
-        .loremresults_container {
-            width:70%;
-            height: 200px;
-            border:1px solid black;
-            padding: 0%;
-            min-width:800px;
-            margin:auto;
-            text-align:left;
-            font-size: 10px;
-            font-weight: bold;
-            overflow:auto;
-
-        }
-
-        .blocktext {
-            margin-left: auto;
-            margin-right: auto;
-            width: 20em;
-        }
-
-        .container .blocktext {
-            margin-left: auto;
-            margin-right: auto;
-            width:80%;
-        }
-
-        .container .content .blocktext {
-            margin-left: auto;
-            margin-right: auto;
-            width:80%;
-            text-align: left;
-        }
-
-        .user_results_container {
-            text-align: left;
-            font-size: 12px;
-            border:1px solid black;
-            padding: 1em;
-            height:500px;
-            width:46%;
-            float: left;
-            border-radius: 15px;
-            overflow: auto;
-            margin: 1em;
-        }
-
-        footer {
-            position: fixed;
-
-            left: 0;
-
-            right: 0;
-
-            bottom: 0;
-
-        }
-
-        nav {
-            margin:auto;
-            text-align: center;
-        }
-
-        div.container {
-            overflow: auto;
-            width: 100%
-        text-align: center;
-            border-radius: 15px;
-        }
-
-        div.container {
-            overflow: auto;
-            width: 100%
-            text-align: center;
-            border-radius: 15px;
-        }
-
-        div img {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        .outer {
-            width: 50%;
-            height: 40%;
-            -webkit-box-shadow: 0px 0px 10px #4d4d4d;
-            -moz-box-shadow: 0px 0px 10px #4d4d4d;
-            box-shadow: 0px 0px 10px #4d4d4d;
-            -webkit-border-radius: 8px;
-            -moz-border-radius: 8px;
-            -khtml-border-radius: 8px;
-            border-radius: 8px;
-            border:solid white 3px;
-            overflow: hidden;
-            padding: 10px;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .image {
-            -webkit-border-radius: 8px;
-            -moz-border-radius: 8px;
-            -khtml-border-radius: 8px;
-            border-radius: 8px;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-        }
-
-        .button {
-            display:block;
-            text-align: center;
-            margin-left: auto;
-            margin-right:auto;
-        }
-
-    </style>
-
-        {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
         @yield('head')
     </head>
 
@@ -193,12 +26,12 @@
         <div class="image">
             <img
                     class="displayed"
-                    src="/assets/images/logo.png"
+                    src="/assets/images/developer's best friend logo.png"
                     style='height:auto; width:100%;display:block;margin-left: auto;margin-right:auto'
                     alt='user and text generator app logo'>
         </div>
     </div>
-
+    <br>
     <nav>
         <a href='/'>&larr; Home</a>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,26 @@
-@extends('layouts.master')
+<!doctype html>
 
-<!---    <head>
+<html>
+    <head>
+
         <title>Developer's Best Friend</title>
-
+        <meta charset='utf-8'>
         <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/readable/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/welcome.css">
+    </head>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>--->
-@section('title')
-    Developer's Best Friend
-@stop
-
-
-{{--
-This `head` section will be yielded right before the closing </head> tag.
-Use it to add specific things that *this* View needs in the head,
-such as a page specific styesheets.
---}}
-@section('head')
-
-@stop
-
-@section('content')
-        <div class="container">
-            <div class="content">
-                <div class="title"><h1>Developer's Best Friend</h1></div>
+    <body>
+    <header>
+        <img
+                class="title"
+                src="/assets/images/developer's best friend logo.png"
+                alt='developers best friend logo'>
+        </div>
+    </header>
+    <content style="text-align: center; width: 80%">
+            <div class="container">
                 <h2>Lorem Ipsum Generator</h2>
                 <div class="blocktext">
                 <p>Lorem ipsum is filler text that is used in place of real text while a project is in production.  Use this app to generate up to 100 paragraphs of fake Latin filler text! Amazing!</p>
@@ -65,9 +32,9 @@ such as a page specific styesheets.
                 </div>
                 <a href="rand-user">Generate Random Users!</a>
             </div>
-        </div>
-@stop
-
-@section('body')
-
-@stop
+    </content>
+    <footer style="position: fixed;left: 0;right: 0;bottom: 0;">
+        &copy; {{ date('Y') }}
+    </footer>
+    </body>
+</html>
